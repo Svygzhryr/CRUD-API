16cb2df78a4ee1cd1fb0111f2a6822a095d18d22
Svygzhryr <pamixy@gmail.com>
2024-02-09 16:11:06 +0300

    other: add tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import * as http from "http";
+import { handleGet, handleDelete, handlePost, handlePut } from "./methods";
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case "GET":
+      handleGet(req, res);
+      break;
+    case "POST":
+      handlePost(req, res);
+      break;
+    case "PUT":
+      handlePut(req, res);
+      break;
+    case "DELETE":
+      handleDelete(req, res);
+      break;
+  }
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.write(JSON.stringify({ message: "Greetings from the server.." }));
+  res.write(JSON.stringify({ message: "Greetings from server!" }));
   res.end();
 });
 
---
+++ src/methods.ts
@@ -0,0 +1,9 @@
+import * as http from "http";
+
+export const handleGet = (req, res) => {};
+
+export const handlePost = (req, res) => {};
+
+export const handlePut = (req, res) => {};
+
+export const handleDelete = (req, res) => {};
